f12831f59806c6a3c01b8e954e1b8abdf6cb0cfb
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-10-04 23:22:26 -0500

    Changing-data-on-the-json-server

--- Practice-DH/exercise-with-json-server/src/App.jsx
+++ Practice-DH/exercise-with-json-server/src/App.jsx
@@ -2,38 +2,64 @@ import './App.css'
 import axios from "axios"
 import { useState, useEffect } from 'react';
 import { Note } from './components/Note';
+import noteService from './services/note.js';
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("effect");
-    axios.get('http://localhost:3001/notes')
-    .then(response =>{
-        console.log('Promise fulfilled');
-        setNotes(response.data)
+    noteService.getAll()
+    .then(initialNotes =>{
+        setNotes(initialNotes);
     })
 
   }, [])
 
-  const addNote = (e) =>{
-      e.preventDefault()
-      const noteObject = {
-          content: newNote,
-          date: new Date().toISOString,
-          important: Math.random() < 0.5,
-          id: notes.length + 1,
-      }
-      setNotes([
-        ...notes, noteObject
-      ])
-      setNewNote("");
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() < 0.5,
+    }
+    noteService.create(noteObject)
+      .then(noteCreated => {
+        setNotes([...notes, noteCreated]);
+        setNewNote("");
+      })
+
+
   }
 
-  const handleNoteChange = (e) =>{
-      setNewNote(e.target.value)
+  const toggleImportanceOf = (id) =>{
+      const note = notes.find(n => n.id === id);
+      const changedNote = {...note, important: !note.important}
+
+      noteService.update(id, changedNote)
+        .then(updatedNote =>{
+            setNotes(notes.map( note => note.id !== id?note: updatedNote));
+        }).catch(error=>{
+            console.log(error);
+            alert(
+              `the note '${note.content}' was already deleted from server`
+            )
+            setNotes(notes.filter(n => n.id !== id))
+            }
+        )
+  }
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value)
+  }
+
+  const handleDeleteNote = (id) =>{
+      noteService.deleteNote(id)
+        .then(answerDelete =>{
+            console.log(answerDelete);
+        })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
@@ -41,25 +67,29 @@ function App() {
   return (
 
     <div>
-      <h1>Notes</h1>
-      <div>
-          <button onClick={()=>{
-            setShowAll(!showAll)
-          }}>
-              Show {showAll? "important": "all"} notes
-          </button>
+      <h1>Notes✍🏾</h1>
+      <div className='mt-3'>
+        <button onClick={() => {
+          setShowAll(!showAll)
+        }}>
+          Show {showAll ? "important notes🚨" : "all notes 📖"}
+        </button>
       </div>
-      <ul className='list_notes'>
-          {notesToShow.map(note =>{
-              return <Note key={note.id} content={note.content}/>
-          })}
+      <ul className='list_notes mt-4'>
+        {notesToShow.map(note => {
+          return <Note key={note.id} note={note} toggleImportance={()=> toggleImportanceOf(note.id)} handleDeleteNote={handleDeleteNote} />
+        })}
       </ul>
-        <form onSubmit={addNote}>
-          <div className='cont'>
-            <input onChange={handleNoteChange} value={newNote}/>
+      <form onSubmit={addNote}>
+        <div className='row align-items-center'>
+          <div className="col-7">
+            <input type="text" className="form-control" onChange={handleNoteChange} value={newNote} />
+          </div>
+          <div className="col-auto">
             <button type="submit">Save</button>
           </div>
-        </form>
+        </div>
+      </form>
     </div>
   )
 }
--- Practice-DH/exercise-with-json-server/src/components/Note.jsx
+++ Practice-DH/exercise-with-json-server/src/components/Note.jsx
@@ -1,6 +1,13 @@
-export function Note(props){
+export function Note({note, toggleImportance, handleDeleteNote}){
+
+    const label = note.important?
+        'make not important': 'make important';
 
     return(
-        <li>{props.content}</li>
+        <li className="mt-1">
+            {note.content}
+            <button className="mx-2" onClick={toggleImportance}>{label}</button>
+            <button className="btn btn-danger" onClick={()=>handleDeleteNote(note.id)}>Delete</button>
+        </li>
     )
 }
